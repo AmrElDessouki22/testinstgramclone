e23b9aff629f6dabe0a75eaae4defc5c06e4309a
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 19:08:59 +0300

    Add src/components/ui/Header.tsx

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => (
+  <header className="bg-white shadow-md p-4">
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold">Instagram Clone</h1>
+    </div>
+  </header>
+);
